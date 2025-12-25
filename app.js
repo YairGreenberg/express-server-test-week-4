e693e4962258db9873b7d8d0f7c465630757ec0e
yair greenberg <yair290899@gmail.com>
2025-12-25 08:34:47 +0200

    ceange port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const PORT = 3030;
 
 
 
-app.listen(port,()=>{
-    console.log(`server is running on http://localhost:${port}`);
+app.listen(PORT,()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
 });
 
